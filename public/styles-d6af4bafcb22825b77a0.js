3a4a0aab2654ef3ce606dbe99f5fce2b1a02c420
Sagar Jain <sagar@zitto.com>
2019-09-19 17:25:25 +0530

    fix: added wp inline images plugin

--- public/styles-d6af4bafcb22825b77a0.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{452:function(n,o,c){},453:function(n,o,c){},454:function(n,o,c){},473:function(n,o,c){},474:function(n,o,c){},475:function(n,o,c){},477:function(n,o,c){},479:function(n,o,c){},482:function(n,o,c){},602:function(n,o,c){}}]);
-//# sourceMappingURL=styles-d6af4bafcb22825b77a0.js.map
