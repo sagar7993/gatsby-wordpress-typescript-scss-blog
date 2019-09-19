f39f970282aa2c0350166b3949ee6b34c34b76b6
Sagar Jain <sagar@zitto.com>
2019-09-19 17:27:38 +0530

    fix: updated public folder

---
+++ public/component---node-modules-gatsby-plugin-offline-app-shell-js-278f0d87d799c5fe9f4e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{398:function(e,t,n){"use strict";var u=n(37);t.__esModule=!0,t.default=void 0;var o=u(n(91)),r=u(n(1)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-278f0d87d799c5fe9f4e.js.map
