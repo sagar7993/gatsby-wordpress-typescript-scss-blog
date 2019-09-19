fd097a267edb087d4b43157272ba74b54f03a5f9
Sagar Jain <sagar@zitto.com>
2019-09-19 16:47:54 +0530

    fix: multiple bug fixes + image and text customer view

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,11 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 require('dotenv').config({
 	path: `.env.${process.env.NODE_ENV}`
 });
 
 module.exports = {
 	siteMetadata: {
-		title: 'My Demo Blog',
-		description: 'This is a demo blog',
+		title: `${process.env.GATSBY_SITE_NAME}`,
+		description: `${process.env.GATSBY_SITE_DESCRIPTION}`,
 		author: `@${process.env.GATSBY_SITE_AUTHOR}`,
 		siteUrl: `${process.env.GATSBY_SITE_URL_PROTOCOL}://${process.env.GATSBY_SITE_URL_PATH}`
 	},
@@ -30,13 +24,13 @@ module.exports = {
 		{
 			resolve: 'gatsby-plugin-manifest',
 			options: {
-				name: 'gatsby-starter-default',
-				short_name: 'starter',
+				name: `${process.env.GATSBY_SITE_NAME}`,
+				short_name: `${process.env.GATSBY_SITE_NAME}`,
 				start_url: '/',
 				background_color: process.env.GATSBY_PRIMARY_ACCENT_COLOR,
 				theme_color: process.env.GATSBY_PRIMARY_ACCENT_COLOR,
 				display: 'minimal-ui',
-				icon: 'src/images/gatsby-icon.png'
+				icon: 'src/images/logo.png'
 			},
 		},
 		{
@@ -89,15 +83,10 @@ module.exports = {
 		  resolve: 'gatsby-plugin-google-analytics',
 		  options: {
 		    trackingId: process.env.GATSBY_GOOGLE_ANALYTICS,
-		    // Defines where to place the tracking script - 'true' in the head and 'false' in the body
 		    head: false,
-		    // Setting this parameter is optional
 		    anonymize: true,
-		    // Setting this parameter is also optional
 		    respectDNT: true,
-		    // Avoids sending pageview hits from custom paths
 		    exclude: ["/preview/**", "/do-not-track/me/too/"],
-		    // Delays sending pageview hits on route update (in milliseconds)
 		    pageTransitionDelay: 0,
 		  }
 		},
