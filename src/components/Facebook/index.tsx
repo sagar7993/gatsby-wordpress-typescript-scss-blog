c1ab29a7aea722543dd92277a71dd7eb3fcbf742
Sagar Jain <sagar@zitto.com>
2019-09-20 17:11:32 +0530

    feat: added customizations for twitter and facebook

---
+++ src/components/Facebook/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Facebook = () => {
+	return (
+		<div className="facebook-container">
+			<div id="fb-root"></div>
+		</div>
+	);
+};
+
+export default Facebook;
---
+++ src/components/Twitter/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Twitter = () => {
+	return (
+		<div className="twitter-container">
+			<div id="fb-root"></div>
+		</div>
+	);
+};
+
+export default Twitter;
