8408a6e80cc19b45739267c65dece29000d06bcb
Sagar Jain <sagar@zitto.com>
2019-09-21 10:21:19 +0530

    fix: updated public folder

---
+++ public/styles-6b0277b2010be0577591.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{458:function(n,o,c){},459:function(n,o,c){},460:function(n,o,c){},479:function(n,o,c){},480:function(n,o,c){},481:function(n,o,c){},483:function(n,o,c){},485:function(n,o,c){},488:function(n,o,c){},608:function(n,o,c){}}]);
+//# sourceMappingURL=styles-6b0277b2010be0577591.js.map
