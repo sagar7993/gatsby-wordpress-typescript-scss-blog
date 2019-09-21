14d8744aa35aa7d4c70a1c672cc5db23e53b832b
Sagar Jain <sagar@zitto.com>
2019-09-21 17:52:21 +0530

    fix: updated public folder

--- public/sw.js
+++ public/sw.js
@@ -27,13 +27,13 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9545511369df5d610483.js"
+    "url": "webpack-runtime-c2f0e463f4fb95e8305e.js"
   },
   {
     "url": "commons.2229e8780a1c8d1635d0.css"
   },
   {
-    "url": "commons-b7d976b9aef20b6d0689.js"
+    "url": "commons-ec901bea76ab822a9ec4.js"
   },
   {
     "url": "app-db059d9ef05dfebb9351.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "53bded41ec01f30f071578620171083f"
+    "revision": "15c0f595d0f5c215d9a7cf41b827b48d"
   },
   {
     "url": "styles.aafcee07cdeaed2d3549.css"
@@ -52,53 +52,53 @@ self.__precacheManifest = [
     "url": "component---src-templates-blog-posts-tsx-a704d1f6fd85080b7fd2.js"
   },
   {
-    "url": "styles-24faf0dc9bbcd00283f6.js"
+    "url": "styles-5f73d56f754ba652e1f2.js"
   },
   {
     "url": "page-data/posts/2/page-data.json",
-    "revision": "2be15d8ec3207380b061e085285bae15"
+    "revision": "de2e456bba26ba354a96aeda549c37cd"
   },
   {
     "url": "page-data/posts/3/page-data.json",
-    "revision": "57363e95641206f4bd889c9d8d7c48a2"
+    "revision": "749fe2f1fb04c288737fdc0fae35b682"
   },
   {
     "url": "page-data/posts/page-data.json",
-    "revision": "cf1d503a5305136bbbdb6b4d94038053"
+    "revision": "e0f67abc1d4bb12fc25dc8276263229a"
   },
   {
     "url": "component---src-templates-blog-post-tsx-980b242427698f2bb988.js"
   },
   {
     "url": "page-data/post/this-is-my-1st-post/page-data.json",
-    "revision": "4f431000ee0535ccc356f643c0e91b8f"
+    "revision": "ffd2dbad8194059664dcb2d2aa4b27b8"
   },
   {
     "url": "page-data/post/this-is-my-2nd-post/page-data.json",
-    "revision": "2cf9c706c57118431739162a5fba5732"
+    "revision": "3364e42e84cd16267c6d908163292725"
   },
   {
     "url": "page-data/post/this-is-my-3rd-post/page-data.json",
-    "revision": "aa778035e024a05adce0122a561b4ac6"
+    "revision": "f77abf0c84ec00deb118ec8204d4ba5b"
   },
   {
     "url": "page-data/post/this-is-my-4th-post/page-data.json",
-    "revision": "e14e8c9a48183b583a974595773bca3d"
+    "revision": "f7ae170152c7d2e752dc612ab89915b5"
   },
   {
     "url": "page-data/post/this-is-my-5th-post/page-data.json",
-    "revision": "c19199f67168ea3e066e746f6d4f7de4"
+    "revision": "2b822fc28bbedce5956a568af65dbdab"
   },
   {
     "url": "page-data/post/this-is-my-6th-post/page-data.json",
-    "revision": "32fb4887db4970bef31364b56750af9d"
+    "revision": "ae24416ff446ece706f83519683d3ecc"
   },
   {
-    "url": "component---src-pages-about-tsx-589effc09b7ac9ec8954.js"
+    "url": "component---src-pages-about-tsx-97585539b15aa50d8c74.js"
   },
   {
     "url": "page-data/about/page-data.json",
-    "revision": "0d46b8d8620ad2999c508b13fa6e5926"
+    "revision": "c0f90064b3d927a23b0bd46e74395fc8"
   },
   {
     "url": "manifest.webmanifest",
