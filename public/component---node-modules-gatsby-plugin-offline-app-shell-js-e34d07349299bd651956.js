3aa23ad8abae6eb42aaa9c0057f84a46bf092457
Sagar Jain <sagar@zitto.com>
2019-09-24 14:45:36 +0530

    fix: updated public folder for release

---
+++ public/component---node-modules-gatsby-plugin-offline-app-shell-js-e34d07349299bd651956.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{408:function(e,t,n){"use strict";var u=n(38);t.__esModule=!0,t.default=void 0;var o=u(n(92)),r=u(n(1)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
+//# sourceMappingURL=component---node-modules-gatsby-plugin-offline-app-shell-js-e34d07349299bd651956.js.map
---
+++ public/styles-e8a6d522a86a9d07b4fe.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{463:function(n,o,c){},482:function(n,o,c){},483:function(n,o,c){},484:function(n,o,c){},486:function(n,o,c){},488:function(n,o,c){},491:function(n,o,c){},611:function(n,o,c){}}]);
+//# sourceMappingURL=styles-e8a6d522a86a9d07b4fe.js.map
